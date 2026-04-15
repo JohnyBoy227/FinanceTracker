28c6527b39e7cf5a4a67e6421d080f8906ce452d
jxm1618 <jxm1618@student.bham.ac.uk>
2026-04-15 12:34:18 +0100

    sqlite database added

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
