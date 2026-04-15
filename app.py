30405022dc251b236591e81a6b5aa4c3e2b1b83d
jxm1618 <jxm1618@student.bham.ac.uk>
2026-04-15 14:21:33 +0100

    expenses table added

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ with app.app_context():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    expenses = Expense.query.order_by(Expense.date.desc(), Expense.id.desc()).all()
+    return render_template("index.html", expenses=expenses)
 
 @app.route("/add", methods=['POST'])
 def add_expense():
